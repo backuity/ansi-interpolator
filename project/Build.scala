1e29d6b0c192e3f04ee8c6356b310644859b20ac
Bruno Bieth <biethb@gmail.com>
2014-11-23 09:56:34 +0100

    add release settings

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,15 @@
 import sbt._
 import Keys._
+import sbtrelease.ReleasePlugin
 
 object BuildSettings {
   val buildSettings = Seq(
-    organization := "org.backuity.ansi",
+    organization := "org.backuity",
     version := "0.1-SNAPSHOT",
     scalaVersion := "2.11.4",
+
+    scalacOptions ++= Seq("-deprecation", "-unchecked"),
+
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
@@ -16,9 +20,27 @@ object BuildSettings {
     },
     publishMavenStyle := true,
     publishArtifact in Test := false,
+
+    // replace publish by publishSigned
+    publish := com.typesafe.sbt.pgp.PgpKeys.publishSigned.value,
+
     pomIncludeRepository := { _ => false},
 
-    licenses := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil
+    homepage := Some(url("https://github.com/backuity/ansi-interpolator")),
+    licenses := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
+
+    pomExtra :=
+      <scm>
+        <url>git@github.com:backuity/ansi-interpolator.git</url>
+        <connection>scm:git:git@github.com:backuity/ansi-interpolator.git</connection>
+      </scm>
+        <developers>
+          <developer>
+            <id>backuitist</id>
+            <name>Bruno Bieth</name>
+            <url>https://github.com/backuitist</url>
+          </developer>
+        </developers>
   )
 }
 
@@ -40,7 +62,7 @@ object AnsiInterpolatorBuild extends Build {
           // Tests
           "org.backuity" %% "matchete" % "1.10" % "test",
           "com.novocode" % "junit-interface" % "0.11" % "test")
-    )
+    ) ++ ReleasePlugin.releaseSettings
   )
 
   lazy val tests = Project("tests",
---
+++ project/plugins.sbt
@@ -0,0 +1,5 @@
+
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.4")
+
+// needed to publish on OSS
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
